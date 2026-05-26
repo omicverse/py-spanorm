5af28134d969ad4c24405f8a10df28dd13f0393b
Zac-lin <18960339395@163.com>
2026-05-27 03:00:56 +0800

    Add .gitignore, remove tracked build/cache files

--- SpaNorm-ref/tests/testthat/test-fastSizeFactors.R
+++
@@ -1,7 +0,0 @@
-test_that("fastSizeFactors works", {
-  suppressPackageStartupMessages(library(SpatialExperiment))
-  data(HumanDLPFC)
-
-  expect_error(filterGenes(HumanDLPFC[, 0]), "ncol") # ensure checkSPE is used
-  expect_equal(length(fastSizeFactors(HumanDLPFC)$sizeFactor), ncol(HumanDLPFC))
-})
